90e7db3d45ef9ac584446b945bb9024b72623d36
ruben <rust.ruben@gmail.com>
2019-11-13 16:49:57 +0100

    début test

---
+++ starchProject-JPA/src/test/java/sopra/formation/TestWithJpa.java
@@ -0,0 +1,14 @@
+package sopra.formation;
+
+import sopra.formation.repository.IUtilisateurRepository;
+
+public class TestWithJpa {
+
+	public static void main(String[] args) {
+		IUtilisateurRepository utilisateurRepo = Singleton.getInstance().getUtilisateurRepo();
+		
+		
+
+	}
+
+}
