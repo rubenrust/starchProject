c308a69a00bc97bfa08a6a375825dd21b9c27de6
ruben <rust.ruben@gmail.com>
2019-11-14 17:10:52 +0100

    rajout groupe repo

--- starchProject-JPA/src/main/java/sopra/formation/repository/IGroupeRepository.java
+++ starchProject-JPA/src/main/java/sopra/formation/repository/IGroupeRepository.java
@@ -1,7 +1,10 @@
 package sopra.formation.repository;
 
+import java.util.List;
+
 import sopra.formation.Groupe;
 
 public interface IGroupeRepository extends IRepository <Groupe,Long>{
+	
 
 }
--- starchProject-JPA/src/main/java/sopra/formation/repository/jpa/GroupeRepositoryJpa.java
+++ starchProject-JPA/src/main/java/sopra/formation/repository/jpa/GroupeRepositoryJpa.java
@@ -130,4 +130,5 @@ public class GroupeRepositoryJpa implements IGroupeRepository {
 
 	}
 
+
 }
