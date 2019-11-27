c166080b54bcba8be1d79e4c735b9ce3a80edd9c
Theo <ducostheo1@gmail.com>
2019-11-27 17:38:55 +0100

    rouge

--- starchProject-boot/src/main/java/fr/starchProject/repository/ICommentaireRepository.java
+++ starchProject-boot/src/main/java/fr/starchProject/repository/ICommentaireRepository.java
@@ -3,6 +3,8 @@ package fr.starchProject.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import fr.starchProject.model.Commentaire;
 
