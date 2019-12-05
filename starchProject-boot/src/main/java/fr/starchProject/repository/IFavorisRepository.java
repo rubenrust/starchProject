966059e7a9d8a352d4c76f227f0705fe5c3c5b54
Oriane <orianegalmar@gmail.com>
2019-12-05 17:03:26 +0100

    qsfqe

--- starchProject-boot/src/main/java/fr/starchProject/repository/IFavorisRepository.java
+++ starchProject-boot/src/main/java/fr/starchProject/repository/IFavorisRepository.java
@@ -1,5 +1,7 @@
 package fr.starchProject.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +11,5 @@ import fr.starchProject.model.Favoris;
 public interface IFavorisRepository extends JpaRepository<Favoris, Long>{
 
 	@Query("select uf from Utilisateur u join u.favoris uf where u.id = :id")
-	Favoris findByUtilisateurId(@Param("id") Long id);
+	List<Favoris> findByUtilisateurId(@Param("id") Long id);
 }
