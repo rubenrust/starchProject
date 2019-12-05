592519167e0b8f391f40f28f5998aba82cb1849d
Arnaud <frfmichel@gmail.com>
2019-12-05 12:02:12 +0100

    commit de mwa

--- starchProject-boot/src/main/java/fr/starchProject/web/EntrepriseController.java
+++ starchProject-boot/src/main/java/fr/starchProject/web/EntrepriseController.java
@@ -3,6 +3,7 @@ package fr.starchProject.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import fr.starchProject.repository.IEvenementRepository;
 
 @RestController
 @RequestMapping("/entreprise")
+@CrossOrigin("*")
 public class EntrepriseController {
 	
 	@Autowired
