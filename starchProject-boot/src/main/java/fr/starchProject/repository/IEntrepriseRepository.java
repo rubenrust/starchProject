281931adcf2d1a46185118548d054157a6ecbf00
Theo <ducostheo1@gmail.com>
2019-12-05 18:15:32 +0100

    change query entrepirse utilisateur

--- starchProject-boot/src/main/java/fr/starchProject/repository/IEntrepriseRepository.java
+++ starchProject-boot/src/main/java/fr/starchProject/repository/IEntrepriseRepository.java
@@ -2,10 +2,14 @@ package fr.starchProject.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import fr.starchProject.model.Entreprise;
 
 
 public interface IEntrepriseRepository extends JpaRepository<Entreprise, Long>{
 	
+	@Query("select e from Entreprise e left join e.collaborateurs u where u.id = :id")
+	Entreprise findEntrepriseByUtilisateurId(@Param("id") Long id);
 }
