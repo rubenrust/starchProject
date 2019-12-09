e0cd8b0727d660a3268f321757416422005db6cf
Theo <ducostheo1@gmail.com>
2019-12-09 16:58:54 +0100

    nom event en +

--- starchProject-boot/src/main/java/fr/starchProject/model/NomEvenement.java
+++ starchProject-boot/src/main/java/fr/starchProject/model/NomEvenement.java
@@ -1,5 +1,5 @@
 package fr.starchProject.model;
 
 public enum NomEvenement {
-	Foot_en_salle, Escape_game, Bowling, Laser_game, Boire_un_verre, Musee;
+	Foot_en_salle, Escape_game, Bowling, Laser_game, Musee, Football, Basketball, Handball, Volleyball, Spectacle, Jeu_de_piste, Olympiades, Virtual_escape_game
 }
